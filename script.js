cd36b32e51d27a6fd544be2842b5315f7383e017
Dhruv <dapatel700@gmail.com>
2023-03-12 17:32:04 -0700

    rewrote api function with different api URL and key

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+var apiKey = "ad0f41660e1acc4acc6a263d5cc5a854"
+var displayWeather = document.getElementById('display-weather')
 
+displayWeather.addEventListener('click',function(event){
+    event.preventDefault()
+})
 
-
+function getForecast(city){
+    var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`
+    fetch(apiUrl)
+    .then( response => response.json())
+    .then(apiData => 
+        console.log(apiData)
+    )
