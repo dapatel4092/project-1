e1be9a06ba4294483cd3557fc05bce6b857b557a
Dhruv <dapatel700@gmail.com>
2023-03-12 17:55:56 -0700

    javascript modification

--- refapiscript.js
+++ refapiscript.js
@@ -48,3 +48,20 @@ fetch('https://openweatherapp.p.rapidapi.com/', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));  */
+
+
+	var apiKey = "ad0f41660e1acc4acc6a263d5cc5a854"
+var displayWeather = document.getElementById('display-weather')
+
+displayWeather.addEventListener('click',function(event){
+    event.preventDefault()
+})
+
+
+function getForecast(city){
+    var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`
+    fetch(apiUrl)
+    .then( response => response.json())
+    .then(apiData => 
+        console.log(apiData)
+    )}
