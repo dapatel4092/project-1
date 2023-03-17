e585b1337d8b42210a03f2cbf95d5b14a8dcafdc
Megan Glorioso <meganglorioso2016@gmail.com>
2023-03-16 17:57:41 -0700

    typos

--- script.js
+++ script.js
@@ -219,7 +219,7 @@ function loadIsland(island) {
   islandName.textContent = getIslandName(island).name;
   islandOrigin.textContent = getIslandName(island).origin;
   console.log(islandImage);
-  islandImage.attributes.src.textContent = `images/${getIslandName(island).link}.jpg`
+  islandImage.attributes.src.textContent = `./images/${getIslandName(island).link}.jpg`
   
 
 
@@ -235,9 +235,9 @@ function loadIsland(island) {
 
       
       
-      image1.attributes.src.textContent = `images/${getImage1(island).link}.jpg`
-      image2.attributes.src.textContent = `images/${getImage2(island).link}.jpg`
-      image3.attributes.src.textContent = `images/${getImage3(island).link}.jpg`
+      image1.attributes.src.textContent = `./images/${getImage1(island).link}.jpg`
+      image2.attributes.src.textContent = `./images/${getImage2(island).link}.jpg`
+      image3.attributes.src.textContent = `./images/${getImage3(island).link}.jpg`
     
       console.log(islandImage);
     
